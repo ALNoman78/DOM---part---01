ce2c76926be992ccb999fd5351a3aeb16849f1ba
ALNoman78 <abdullaalno078@gmail.com>
2024-09-14 19:14:47 +0600

    get element by class name

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,21 @@
 // console.log(document.body)
-let liCollection = document.getElementsByTagName("li")
-for (const li of liCollection) {
-    console.log(li.innerText);
-}
-console.log(liCollection);
+
+//  using get element by tag name
+
+// let liCollection = document.getElementsByTagName("li")
+// for (const li of liCollection) {
+//     console.log(li.innerText);
+// }
+// console.log(liCollection);
+
+
+// using get element by id
+
+const sectionId = document.getElementById("cities")
+// for (const ul of sectionId) {
+//     console.log(ul);
+// }
+console.log(sectionId.innerText);
+
+const destinationTitle = document.getElementById("places")
+console.log(destinationTitle.innerHTML);
