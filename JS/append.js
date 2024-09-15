e2e6344c01a6c783b7c84e1ff1118773b6d17b1c
ALNoman78 <abdullaalno078@gmail.com>
2024-09-15 08:15:49 +0600

    appendChild

--- JS/append.js
+++ JS/append.js
@@ -14,3 +14,18 @@ sections.innerHTML = `
     </ul>
 `
 mainContainer.append(sections)
+
+
+const child = document.getElementById("chid");
+const li = document.createElement("li")
+li.innerText = "Product number 5"
+child.appendChild(li)
+
+const e = document.getElementById("new-element")
+const n = document.createElement("li")
+n.innerText = "Last form added"
+e.appendChild(n)
+n.style.color = "steelblue"
+n.style.fontSize = "1.5rem"
+n.style.fontWeight = "bold"
+n.style.fontFamily = "Poppins"
