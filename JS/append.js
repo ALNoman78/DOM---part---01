49217ac1192072a83ce783650c36dd700598b29c
ALNoman78 <abdullaalno078@gmail.com>
2024-09-15 14:51:05 +0600

    styles and appenedChild

--- JS/append.js
+++ JS/append.js
@@ -29,3 +29,18 @@ n.style.color = "steelblue"
 n.style.fontSize = "1.5rem"
 n.style.fontWeight = "bold"
 n.style.fontFamily = "Poppins"
+
+
+
+const addAppend = document.querySelector(".add-appendChild")
+const add2 = document.createElement("li")
+add2.innerText = "Added by Abdullah AL Noman"
+add2.style.fontSize  = "32px"
+add2.style.fontWeight = "700"
+add2.style.fontFamily = "inter"
+add2.style.textTransform = "uppercase"
+add2.style.letterSpacing = "1px";
+add2.classList.add("color")
+add2.style.color = "#fff"
+add2.style.padding = "12px"
+addAppend.append(add2)
